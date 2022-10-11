556015e3cb553fae1ea9c3bcf038bae16b3d1653
Kayzwer Lee <lkyu753@gmail.com>
2022-10-11 11:15:58 +0800

    change activation func

--- Guided Cost Learning/RewardFunction.py
+++ Guided Cost Learning/RewardFunction.py
@@ -7,7 +7,7 @@ class RewardFunction(nn.Module):
         super(RewardFunction, self).__init__()
         self.layers = nn.Sequential(
             nn.Linear(input_size, 128),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(128, 1),
             nn.ReLU()
         )
